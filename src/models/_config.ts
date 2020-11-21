7cba284dfaf05547ba48bf49daa5a860f2f2e4c5
Alabi Sodiq <adisco4420@gmail.com>
2020-11-21 22:12:27 +0100

    base route

--- src/models/_config.ts
+++ src/models/_config.ts
@@ -7,13 +7,13 @@ export const dbConfig = () => {
     mongoose.set('useNewUrlParser', true)
     mongoose.set('useUnifiedTopology', true);
     mongoose.set('useFindAndModify', false);
-
+   
     // Connect to MongoDB
     mongoose.connect(env.MONGODB_URI)
     .then(() => {
       console.log('✌🏾 Successfully connected to MongoDB');
     })
     .catch(err => {
-      console.log(chalk.red.bgBlack.bold('An error occured while conencting to MongoDB'));
+      console.log(chalk.red.bgBlack.bold('An error occured while conencting to MongoDB', err));
     });
 }
