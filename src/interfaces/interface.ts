b9e8e49e242aa6fe6f11cc9fd1f331d5f9ee32a4
Alabi Sodiq <adisco4420@gmail.com>
2020-11-21 20:29:26 +0100

    UserI & UserRequestI interfaces

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,6 +1,20 @@
+import { Request } from 'express'
+
 export interface SampleI {
     _id: string;
     name: string;
     description?: string;
     createdAt: number;
+}
+export interface UserI {
+    _id: string;
+    email: string;
+    firstName?: string;
+    lastName?: string;
+    isVerified?: boolean;
+    password?: string;
+    walletBalance? : number
+}
+export interface UserRequestI extends Request {
+    user?: UserI;
 }
