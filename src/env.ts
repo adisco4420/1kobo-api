8bed07dae568cfab4a5652d27be0fda4b4cc9220
Alabi Sodiq <adisco4420@gmail.com>
2020-11-22 20:18:13 +0100

    update .env with more apiKeys

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config()
 
+const randomStr = 'e98dec370c9252025ed190bfbfa9d196';
+
 const env = {
     MONGODB_URI : process.env.MONGODB_URI,
     JWT_KEY: process.env.JWT_KEY,
-    MAILGUN_KEY : process.env.MAILGUN_KEY
+    MAILGUN_KEY : process.env.MAILGUN_KEY,
+    hostUrl: 'https://onekobo-api.herokuapp.com',
+    rubieVirtualActCallbackUrl: `virtualAct/callback/${randomStr}`,
+    rubiesBankApiKey: process.env.rubiesBankApiKey
 }
 export default env;
