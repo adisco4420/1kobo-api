4b3d78bb1b910eba519a34d8b17590988f5af375
Sodiq Alabi <adisco4420@gmail.com>
2020-12-02 23:12:23 +0100

    update saving validator

--- src/validations/savings.validator.ts
+++ src/validations/savings.validator.ts
@@ -3,8 +3,9 @@ import * as joi from 'joi';
 class SavingsValidator {
     public create = joi.object({
         name : joi.string().required(),
-        type: joi.string().valid('daily', 'weekly', 'monthly').required(),
-        amount: joi.number().min(100).max(5000000).required(),
+        frequency: joi.string().valid('daily', 'weekly', 'monthly').required(),
+        planType: joi.string().valid('bronze', 'silver', 'gold').required(),
+        amount: joi.number().required(),
         duration: joi.number().required(),
         startDate: joi.number().required(),
     })
