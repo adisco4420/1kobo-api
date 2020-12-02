aada436ffa748998fef93390545ba04d2471dbcf
Sodiq Alabi <adisco4420@gmail.com>
2020-12-02 23:11:13 +0100

    rubies bank service

--- src/services/rubiesbank.service.ts
+++ src/services/rubiesbank.service.ts
@@ -1,14 +1,18 @@
-import { UserI } from './../interfaces/interface';
-import env from '../env';
+import { Request, Response} from 'express';
 import axios from 'axios';
+import env from '../env';
+import { RootService, Status } from './_root.service';
 import VirtualactTx from '../controllers/virtualact-tx.control';
+import SavingsService from './savings.service';
+import TransService from './trans.service';
 
-
-class RubiesBankService {
+class RubiesBankService extends RootService {
     private api = 'https://openapi.rubiesbank.io/v1';
     private apikey = env.rubiesBankApiKey;
+    private callbackurl = `${env.hostUrl}/rubies-bank/${env.rubieVirtualActCallbackUrl}`
 
     constructor() {
+        super();
         this.AxoisInterceptor()
     }
     private AxoisInterceptor() {
@@ -18,6 +22,18 @@ class RubiesBankService {
             }
             return config
         })
+    }
+    private simulatedVirtualActCreation(): Promise<any> {
+        const randomAct = `${Date.now()}`.substring(0, 10);
+        const promise = new Promise((resolve) => {
+            const data = {
+                virtualaccountname: 'Sodiq Alabi', 
+                virtualaccount: `${randomAct}`, 
+                amount: "1000",
+            }
+            resolve({data});
+        }) 
+        return promise;
     }
     private getActiveDays = ({type, duration}) => {
         const dayx = type === 'monthly' ?  30 :  type === 'weekly' ? 7 : 1;
@@ -32,9 +48,11 @@ class RubiesBankService {
                     daysactive: this.getActiveDays(body),
                     minutesactive: 30,
                     amount: `${body.amount}`,
-                    callbackurl: `${env.hostUrl}/rubies-bank/${env.rubieVirtualActCallbackUrl}`
+                    callbackurl: this.callbackurl
                 }
-                const { data  } = await axios.post(`${this.api}/createvirtualaccount`, payload); 
+                // const { data  } = await axios.post(`${this.api}/createvirtualaccount`, payload); 
+
+                const { data } = await this.simulatedVirtualActCreation()
                 const { _id: planId, planType, amount, userId } = body;
                 const { virtualaccount, virtualaccountname} = data
                 resolve(data)
@@ -45,6 +63,38 @@ class RubiesBankService {
             }
         })
         return promise
+    }
+    virtualActCallBack = async (req: Request, res: Response) => {
+        try {
+            console.log('=====Virtual Act Callback==='); 
+            const { amount, craccount: virtualaccount, originatorname, originatoraccountnumber, paymentreference  } = req.body;
+            const amountNo = Number(amount);
+            const virtalActTxAwait = await VirtualactTx.getOne({virtualaccount});
+            if(!virtalActTxAwait) throw 'virtualact Txes not found';
+
+            const virtalActTx = this.jsonize(virtalActTxAwait);            
+            const { planId, planType, userId} = virtalActTx;
+            if(planType === 'savings') {
+                await SavingsService.fundPlan({planId, amount: amountNo})
+            } else if(planType === 'investments') {
+                console.log('investment funding...');
+            }
+            const transType = planType === 'savings' ? 'fundSaving': planType === 'investments' ? 'fundInvestment': 'fundWallet'
+            TransService.create({
+                amount: amountNo,
+                userId,
+                type: transType,
+                desc: `Fund ${planType} plan via bank transfer`,
+                status: 'success',
+                createdDate: Date.now(),
+                source: 'bank-transfer',
+                transId: paymentreference
+            })
+
+            this.sendResponse({status: Status.SUCCESS, data: 'Account Funding Successful'}, res);
+        } catch (error) {
+            this.sendResponse({status: Status.ERROR, data: error}, res)
+        }
     }
 }
 export default new RubiesBankService
